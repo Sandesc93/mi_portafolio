15ea219bb0bd9eabadb701ba3b83dc7bf22bec2b
Dennis Sandoval <san.esc.93.14@gmail.com>
2022-09-08 17:14:33 -0500

    mejorando

--- JS.js
+++ JS.js
@@ -18,32 +18,32 @@ container.addEventListener('click', function (e) {
 
 
 // const secPort = document.querySelector('portafolio')
-// const servPrev = document.getElementById('sprev')
-// const servNext = document.getElementById('snext')
-// const porfPrev =document.getElementById('pprev')
-// const porfNext= document.getElementById('pnext')
-// const servicios = document.querySelector('.servicios__card')
-// const portafolio= document.querySelector('.porf__contenedor')
+const servPrev = document.getElementById('sprev')
+const servNext = document.getElementById('snext')
+const porfPrev =document.getElementById('pprev')
+const porfNext= document.getElementById('pnext')
+const servicios = document.querySelector('.card')
+const portafolio= document.querySelector('.porf__contenedor')
 
-// servPrev.addEventListener('click', ()=>{ 
-//        servicios.scrollLeft = -200
-//       console.log('hola')
-//    })
+servPrev.addEventListener('click', ()=>{ 
+       servicios.scrollLeft -= 300;
+      console.log('hola')
+   })
    
-//    servNext.addEventListener('click', () => {
-//        servicios.scrollLeft +=200
-//        console.log('hola')
-//    })
+   servNext.addEventListener('click', () => {
+       servicios.scrollLeft += 300;
+       console.log('hola')
+   })
    
-//    porfPrev.addEventListener('click' , ()=>{
-//        portafolio.scrollLeft -=200
-//        console.log('hola')
-//    })
+   porfPrev.addEventListener('click' , ()=>{
+       portafolio.scrollLeft -=200
+       console.log('hola')
+   })
    
-//    porfNext.addEventListener('click' , ()=>{
-//        portafolio.scrollLeft +=200
-//        console.log('hola')
-//    })
+   porfNext.addEventListener('click' , ()=>{
+       portafolio.scrollLeft +=200
+       console.log('hola')
+   })
 
 
 
