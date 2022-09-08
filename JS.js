ada161da1127941d3ac2aad2fe4df78a57e0c34b
Dennis Sandoval <san.esc.93.14@gmail.com>
2022-09-07 21:58:30 -0500

    versión 1

---
+++ JS.js
@@ -0,0 +1,44 @@
+
+const container = document.querySelector('.navbar__container')
+const menu = document.getElementById('menu')
+
+container.addEventListener('click', function (e) {
+  if(e.target.matches('.menu-open')) {
+    menu.classList.add('activo')
+  }
+
+  if(e.target.matches('.menu-close')) {
+    menu.classList.remove('activo')
+  }
+
+  if (e.target.matches('.menu__link')) {
+    menu.classList.remove('activo')
+  }
+})
+const secPort = document.querySelector('portafolio')
+const servPrev = document.getElementById('sprev')
+const servNext = document.getElementById('snext')
+const porfPrev =document.getElementById('pprev')
+const porfNext= document.getElementById('pnext')
+const servicios = document.querySelector('.servicios__card')
+const portafolio= document.querySelector('.porf__contenedor')
+
+servPrev.addEventListener('click', ()=>{ 
+       servicios.scrollLeft = -200
+      console.log('hola')
+   })
+   
+   servNext.addEventListener('click', () => {
+       servicios.scrollLeft +=200
+       console.log('hola')
+   })
+   
+   porfPrev.addEventListener('click' , ()=>{
+       portafolio.scrollLeft -=200
+       console.log('hola')
+   })
+   
+   porfNext.addEventListener('click' , ()=>{
+       portafolio.scrollLeft +=200
+       console.log('hola')
+   })
